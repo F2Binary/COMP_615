8352819ffa42f49dccbf8f2d4f8bbc03da63b037
Jonathan <cordovaj63@gmail.com>
2023-12-03 15:27:02 -0800

    Created JSON_Helper java file for parsing JSON files into DFAs, config using pom.xml for Maven, revised gitignore to account for other IDEs

--- AutomataProject/Frontend_Helpers/JSON_Helper.java
+++ AutomataProject/Frontend_Helpers/JSON_Helper.java
@@ -1,5 +1,34 @@
 package Frontend_Helpers;
 
+import org.json.JSONObject;
+import org.json.JSONArray;
+import DFA_Equivalence.DFA;
+import DFA_Equivalence.State;
+
 public class JSON_Helper {
 
+    public static DFA parseJSON(String jsonString) {
+        JSONObject jsonObject = new JSONObject(jsonString);
+        DFA dfa = new DFA();
+
+        // Iterate through each state in the JSON object
+        for (String fromStateId : jsonObject.keySet()) {
+            JSONObject transitions = jsonObject.getJSONObject(fromStateId);
+            State fromState = dfa.getState(Integer.parseInt(fromStateId));
+
+            // Iterate through each transition in the state
+            for (String toStateId : transitions.keySet()) {
+                JSONArray inputs = transitions.getJSONArray(toStateId);
+                State toState = dfa.getState(Integer.parseInt(toStateId));
+
+                // Add transitions for each input symbol
+                for (int i = 0; i < inputs.length(); i++) {
+                    char input = inputs.getString(i).charAt(0);
+                    fromState.addTransition(input, toState);
+                }
+            }
+        }
+
+        return dfa;
+    }
 }
--- AutomataProject/Main.java
+++ AutomataProject/Main.java
@@ -1,18 +1,34 @@
-import Frontend_Helpers.TEXT_Helper;
+import Frontend_Helpers.JSON_Helper;
 import DFA_Equivalence.DFA;
 import DFA_Equivalence.DFA_Helper;
 import DFA_Equivalence.Equivalence_Checker;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Testing DFA1 and DFA2 Equivalence Checker.");
 
 		try {
-			String filePath1 = "AutomataProject/Frontend_Helpers/dfa1.txt";
-			String filePath2 = "AutomataProject/Frontend_Helpers/dfa2.txt";
-			DFA dfa1 = TEXT_Helper.readDFAFromFile(filePath1);
-			DFA dfa2 = TEXT_Helper.readDFAFromFile(filePath2);
+
+			// Using TEXT_Helper
+			// String filePath1 = "AutomataProject/Frontend_Helpers/dfa1.txt";
+			// String filePath2 = "AutomataProject/Frontend_Helpers/dfa2.txt";
+			// DFA dfa1 = TEXT_Helper.readDFAFromFile(filePath1);
+			// DFA dfa2 = TEXT_Helper.readDFAFromFile(filePath2);
+
+			// Using JSON_Helper
+			String filePath1 = "AutomataProject/Frontend_Helpers/dfa1.json";
+			String filePath2 = "AutomataProject/Frontend_Helpers/dfa2.json";
+
+			// Read JSON content from files
+			String jsonDFA1 = new String(Files.readAllBytes(Paths.get(filePath1)));
+			String jsonDFA2 = new String(Files.readAllBytes(Paths.get(filePath2)));
+
+			// Parse the JSON content to create DFA objects
+			DFA dfa1 = JSON_Helper.parseJSON(jsonDFA1);
+			DFA dfa2 = JSON_Helper.parseJSON(jsonDFA2);
 
 			// Display each DFA
 			System.out.println("DFA 1:");
